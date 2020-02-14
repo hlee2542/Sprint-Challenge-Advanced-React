94520bdd61fd9d36ed4f8365cc3f7de5d897d9ff
hlee2542 <ou40a9rtbp@gmail.com>
2020-02-14 10:11:54 -0800

    added tests

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,9 +1,21 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { render, fireEvent } from "@testing-library/react";
 import App from './App';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+import 'mutationobserver-shim';
+
+test("renders App without crashing", () => {
+  render(<App />);
+});
+
+test('loading text is there', () => {
+  const {getByText} = render(<App />);
+  const loading = getByText(/loading/i);
+  expect(loading).toBeInTheDocument();
+});
+
+test('dark mode button is there', () => {
+  const {getByText} = render(<App />);
+  const darkMode = getByText(/dark mode/i);
+  expect(darkMode).toBeInTheDocument();
 });
