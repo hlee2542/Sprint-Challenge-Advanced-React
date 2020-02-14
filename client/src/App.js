cdebe830283a29166d156d4e0b83f937938ca2d9
hlee2542 <ou40a9rtbp@gmail.com>
2020-02-14 09:14:13 -0800

    added dark mode

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,32 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
+import './App.css';
 import Member from './Member';
+import useDarkMode from './darkMode';
 
-class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      players: []
-    }
-  }
-  componentDidMount() {
+const App = (props) => {
+  const [players, setPlayers] = useState([]);
+  const [darkMode, setDarkMode] = useDarkMode();
+  const body = document.querySelector('body');
+  useEffect(() => {
     fetch('http://localhost:5000/api/players')
       .then(res => res.json())
-        .then(data => this.setState({players: data}))
-  }
-  render() {
-    return (
-      <div>
-        <button>Dark Mode</button>
-        {this.state.players.length ? this.state.players.map(player => <Member key={player.id} player={player}/>) : 'Loading'}
-      </div>
-    );
-  }
+        .then(data => setPlayers(data));
+  }, []);
+  useEffect(() => {
+    if (darkMode) {
+      body.style.backgroundColor = 'black';
+      body.style.color = 'white';
+    } else {
+      body.style.backgroundColor = 'white';
+      body.style.color = 'black';
+    }
+  }, [darkMode])
+  return (
+    <div>
+      <button onClick={setDarkMode}>Dark Mode</button>
+      {players.length ? players.map(player => <Member key={player.id} player={player}/>) : 'Loading'}
+    </div>
+  );
 }
 
 export default App;
